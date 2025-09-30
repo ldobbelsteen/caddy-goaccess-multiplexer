0c693f55307ef90982a1a669820d3d773663d741
Lukas <lukas.dobbelsteen@essent.nl>
2025-09-30 17:32:02 +0200

    Refactoring

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import subprocess
 
 BASIC_AUTH_USER = os.environ["BASIC_AUTH_USER"]
 BASIC_AUTH_PASSWORD = os.environ["BASIC_AUTH_PASSWORD"]
-LOG_DIR = os.environ["LOG_DIR"]
-GOACCESS_OPTIONS = os.environ["GOACCESS_OPTIONS"].split()
+LOG_DIR = os.environ.get("LOG_DIR", "/logs")
 
 
 def is_authenticated(user, password):
@@ -13,33 +12,31 @@ def is_authenticated(user, password):
     return user == BASIC_AUTH_USER and password == BASIC_AUTH_PASSWORD
 
 
-def list_hostnames() -> list[str]:
+def list_hosts() -> list[str]:
     return []  # TODO
 
 
 @route("/")
 @auth_basic(is_authenticated)
 def index():
-    hostnames = list_hostnames()
+    hosts = list_hosts()
 
-    if len(hostnames) == 0:
-        return "No hostnames found in logs."
+    if len(hosts) == 0:
+        return "No hosts found in logs."
 
-    return "<br>".join(
-        f"<a href='/view/{hostname}'>{hostname}</a>" for hostname in hostnames
-    )
+    return "<br>".join(f"<a href='/view/{host}'>{host}</a>" for host in hosts)
 
 
-@route("/view/<hostname>")
+@route("/view/<host>")
 @auth_basic(is_authenticated)
-def render_goaccess_for_hostname(hostname: str):
-    filter_str = f'"host":"{hostname}"'
+def render_goaccess_for_host(host: str):
+    filter_str = f'"host":"{host}"'
 
     result = subprocess.run(
         [
             "sh",
             "-c",
-            f"zgrep -hF '{filter_str}' -- {LOG_DIR}/*.log {LOG_DIR}/*.log.gz | goaccess --log-format=CADDY --output html {' '.join(GOACCESS_OPTIONS)} -",
+            f"zgrep -hF '{filter_str}' -- {LOG_DIR}/*.log {LOG_DIR}/*.log.gz | goaccess --log-format=CADDY --output html -",
         ],
         stdout=subprocess.PIPE,
         check=True,
